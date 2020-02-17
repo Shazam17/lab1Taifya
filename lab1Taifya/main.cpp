9a2af860fdf96cf3b7730830ccc622c2e729ae90
JohnPower <stkautoru@mail.ru>
2020-02-17 14:17:32 +0700

    Initial Commit

---
+++ lab1Taifya/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab1Taifya
+//
+//  Created by Николай Костин on 17.02.2020.
+//  Copyright © 2020 Николай Костин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
